bc357cdc03794e4d08e55e590aa4b8ce5435af65
Angus Farquhar <42331896+angusfarquhar@users.noreply.github.com>
2020-08-23 08:26:28 +1000

    add prettier and nav component (#36)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Nav from './Nav.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to your epicportfolio!
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Nav />
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Welcome to your epicportfolio!</p>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
