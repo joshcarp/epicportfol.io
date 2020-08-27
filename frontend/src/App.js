1f1405be168138fb589f33d72cdd24d32a94081e
joshcarp <joshuacarpeggiani@gmail.com>
2020-08-28 09:06:52 +1000

    Use propper basic auth in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
 const {LoginRequest} = require('./proto/api_pb.js');
-const {authenticateClient, metadata} = require('./proto/api_grpc_web_pb.js');
+const {authenticateClient} = require('./proto/api_grpc_web_pb.js');
 const auth = new authenticateClient('http://localhost:8081');
 
 
 function Login() {
     const [message, setMessage] = useState("initial value")
     var request = new LoginRequest();
-    var meta = {'authorization': "Basic "+window.btoa('memes')+':'+window.btoa('more_memes')}
+    var meta = {'authorization': "Basic "+window.btoa('memes:more_memes')}
     auth.login(request, meta, function (err, response) {
         console.log(response.getJwt())
         setMessage(response.getJwt())
