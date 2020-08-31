55d9a1449af8682db5821329882a8a3a2ed45ae7
Haohan Liu <the1nonlypanda@gmail.com>
2020-08-31 13:09:29 +1000

    Organise frontend components

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 import { useState } from 'react'
-const { LoginRequest } = require('./proto/api_pb.js');
-const { authenticateClient } = require('./proto/api_grpc_web_pb.js');
+const { LoginRequest } = require('../proto/api_pb.js');
+const { authenticateClient } = require('../proto/api_grpc_web_pb.js');
 const auth = new authenticateClient('http://localhost:8081');
 
 
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react'
-const {RegisterRequest} = require('./proto/api_pb.js');
-const {authenticateClient} = require('./proto/api_grpc_web_pb.js');
+import logo from '../logo.svg';
+import '../App.css';
+import { useState } from 'react'
+const { RegisterRequest } = require('../proto/api_pb.js');
+const { authenticateClient } = require('../proto/api_grpc_web_pb.js');
 const auth = new authenticateClient('http://localhost:8081');
 
 
@@ -21,7 +21,7 @@ function Register() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
+                <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     {message.toString()}
                 </p>
