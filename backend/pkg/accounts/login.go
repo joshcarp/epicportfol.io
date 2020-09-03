8ab88f8cb0bee8d17aa7ab0645ac490ec1fabaf6
Joshua Carpeggiani <32605850+joshcarp@users.noreply.github.com>
2020-09-03 19:27:35 +1000

    WIP: Start the backend for frontend (#44)
    * Start the backend for frontend
    
    * Add login calling from frontend

--- backend/pkg/accounts/login.go
+++ backend/pkg/accounts/login.go
@@ -3,6 +3,9 @@ package accounts
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/joshcarp/it-project/backend/pkg/auth"
 
 	"github.com/joshcarp/it-project/backend/pkg/database"
@@ -17,7 +20,7 @@ func (s *Server) Login(ctx context.Context, req *itproject.LoginRequest) (*itpro
 		return nil, err
 	}
 	if err := database.VerifyUser(s.db, username, password); err != nil {
-		return nil, err
+		return nil, status.Error(codes.PermissionDenied, "Incorrect username or password")
 	}
 	token, err := jwt.Issue(map[string]interface{}{"email": username})
 	return &itproject.LoginResponse{Jwt: token}, err
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 import { useState } from 'react'
 const { LoginRequest } = require('../proto/api_pb.js');
@@ -7,24 +6,15 @@ const { authenticateClient } = require('../proto/api_grpc_web_pb.js');
 const auth = new authenticateClient('http://localhost:8081');
 
 
-function Login() {
+function Login(uname, pwd) {
     const [message, setMessage] = useState("initial value")
     var request = new LoginRequest();
-    var meta = { 'authorization': "Basic " + window.btoa('memes:more_memes') }
+    var meta = { 'authorization': "Basic " + window.btoa(uname+':'+pwd) }
     auth.login(request, meta, function (err, response) {
         console.log(response.getJwt())
         setMessage(response.getJwt())
     })
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    {message.toString()}
-                </p>
-            </header>
-        </div>
-    );
+    return message
 }
 
 
