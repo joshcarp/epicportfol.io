ee094b50ac3b28a5f2a378dc7e5fbc326eecca41
Angus Farquhar <mr.angus.farquhar@gmail.com>
2020-09-06 16:52:55 +1000

    get jwt

--- frontend/src/components/UserLoginForm.js
+++ frontend/src/components/UserLoginForm.js
@@ -1,51 +1,70 @@
-import React from "react";
-const { LoginRequest } = require('../proto/api_pb.js');
-const { authenticateClient } = require('../proto/api_grpc_web_pb.js');
-const auth = new authenticateClient('http://localhost:8081');
+import React from 'react'
+const { LoginRequest } = require('../proto/api_pb.js')
+const { authenticateClient } = require('../proto/api_grpc_web_pb.js')
+const auth = new authenticateClient('http://localhost:8081')
 
 // Component to create form that logs user into their profile and redirects
 // to profile page.
 class UserLoginForm extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             password: '',
-            username: ''
-        };
-        this.handleUname = this.handleUname.bind(this);
-        this.handlepwd = this.handlepwd.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+            username: '',
+        }
+        this.handleUname = this.handleUname.bind(this)
+        this.handlepwd = this.handlepwd.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
     handleUname(event) {
         this.setState({
             username: event.target.value,
-        });
+        })
     }
     handlepwd(event) {
         this.setState({
             password: event.target.value,
-        });
+        })
     }
     handleSubmit(event) {
-        event.preventDefault();
-        var request = new LoginRequest();
-        var meta = { 'authorization': "Basic " + window.btoa(this.state.username + ':' + this.state.password) }
+        event.preventDefault()
+        var request = new LoginRequest()
+        var meta = {
+            authorization:
+                'Basic ' +
+                window.btoa(this.state.username + ':' + this.state.password),
+        }
         auth.login(request, meta, function (err, response) {
-            console.log(err.code, err.message)//, response.getJwt())
+            err != null
+                ? console.log(err.code, err.message)
+                : console.log(response.getJwt())
+            // console.log(err.code, err.message)//, response.getJwt())
         })
     }
     render() {
         return (
             <div className="UserLoginForm">
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="username" value={this.state.username} onChange={this.handleUname} placeholder="Username" />
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleUname}
+                        placeholder="Username"
+                    />
                     <br />
-                    <input type="password" name="password" value={this.state.password} onChange={this.handlepwd} placeholder="Password" />
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handlepwd}
+                        placeholder="Password"
+                    />
                     <input type="submit" value="Submit" />
                 </form>
             </div>
-        );
+        )
     }
 }
 
-export default UserLoginForm;
+export default UserLoginForm
