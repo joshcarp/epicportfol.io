8039c2ab518a199ade9da550af040a16b122226f
Haohan Liu <the1nonlypanda@gmail.com>
2020-09-06 15:14:44 +1000

    Clean up components

--- frontend/src/containers/Homepage.js
+++ frontend/src/containers/Homepage.js
@@ -6,16 +6,6 @@ import Logo from '../assets/logo.svg'
 
 import UserSearchBox from '../components/UserSearchBox'
 import UserLoginForm from '../components/UserLoginForm'
-import Button from '../components/UI/Button/Button'
-import Toolbar from '../components/Navigation/Toolbar/Toolbar'
-import Aux from '../hoc/Aux'
-import SideDrawer from '../components/Navigation/SideDrawer/SideDrawer'
-import classes from './Homepage.css'
-
-
-function sayHello() {
-    alert('You clicked me!');
-}
 
 class Homepage extends Component {
     state = {
@@ -23,50 +13,27 @@ class Homepage extends Component {
     }
 
     sideDrawerClosedHandler = () => {
-        this.setState( { showSideDrawer: false } );
+        this.setState({ showSideDrawer: false });
     }
 
     sideDrawerToggleHandler = () => {
-        this.setState( ( prevState ) => {
+        this.setState((prevState) => {
             return { showSideDrawer: !prevState.showSideDrawer };
-        } );
+        });
     }
 
-    render () {
+    render() {
         return (
             <div className="Homepage">
 
+                <img src={Logo} className="Homepage-logo" alt="logo" />
+                <h4>Find your next Epic Candidate</h4>
+                <UserSearchBox />
 
-                        <Aux>
-                            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-                            <SideDrawer
-                                open={this.state.showSideDrawer}
-                                closed={this.sideDrawerClosedHandler} />
-                            <main className={classes.Content}>
-                                {this.props.children}
-                            </main>
+                <h4>Login to your Epic Portfolio</h4>
+                <UserLoginForm />
 
-                        </Aux>
-
-
-            <img src={Logo} className="Homepage-logo" alt="logo" />
-            <h4>Find your next Epic Candidate</h4>
-            <UserSearchBox />
-
-            <h4>Login to your Epic Portfolio</h4>
-            <UserLoginForm />
-
-            <Button onClick={sayHello}>
-                Login
-            </Button>
-            <a href="https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp" target="_blank">
-                <Button theme = "pink">
-                    Register
-                </Button>
-            </a>
-
-
-        </div>
+            </div>
 
 
 
