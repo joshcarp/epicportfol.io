0dd16c8e95d55c62575172e482c7a1fc531a0487
Haohan Liu <the1nonlypanda@gmail.com>
2020-09-07 15:25:12 +1000

    Fix rebase and App.js routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Homepage from './containers/Homepage'
+import UserProfile from './containers/UserProfile'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import RegisterPage from './containers/RegisterPage'
@@ -8,10 +9,10 @@ export default function App() {
     return (
         <Router>
             <Switch>
-                <Route path="/Register">
+                <Route exact path="/Register">
                     <RegisterPage />
                 </Route>
-                <Route path="/">
+                <Route exact path="/">
                     <Homepage />
                 </Route>
                 <Route exact path="/profile">
