2881d3caba732a5ef9ea23f0f2a21384115265eb
Haohan Liu <the1nonlypanda@gmail.com>
2020-09-26 22:58:31 +1000

    Add User Profile Page skeleton with existing component implementations (#74)
    * Convert to functional component and define preliminary theme for profile cards
    
    * Refactor UserProfile component into UserInfoCard and add responsive design
    
    * Alter ImageBox parameters to look cleaner

--- frontend/src/components/ImageBox.js
+++ frontend/src/components/ImageBox.js
@@ -3,20 +3,29 @@ import './ImageBoxStyle.css'
 import ImageGallery from 'react-image-gallery';
 const images = [
     {
-        original: 'https://picsum.photos/100/100',
-        thumbnal: 'https://picsum.photos/100/100',
+        original: 'https://picsum.photos/id/1/1200/800',
+        thumbnal: 'https://picsum.photos/id/1/100',
     },
     {
-        original: 'https://picsum.photos/100/100',
-        thumbnal: 'https://picsum.photos/100/100',
+        original: 'https://picsum.photos/id/2/1200/800',
+        thumbnal: 'https://picsum.photos/id/2/100',
     },
     {
-        original: 'https://picsum.photos/100/100',
-        thumbnal: 'https://picsum.photos/100/100',
+        original: 'https://picsum.photos/id/3/1200/800',
+        thumbnal: 'https://picsum.photos/id/3/100',
     },
 ]
-export default function ImageBox(){
-    return ( <ImageGallery items={images} />)
-    };
-            
+
+export default function ImageBox() {
+    return (
+        <ImageGallery
+            items={images}
+            showBullets={true}
+            showThumbnails={false}
+            showPlayButton={false}
+            showFullscreenButton={false}
+        />
+    )
+};
+
 
