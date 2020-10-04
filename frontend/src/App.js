dad4b82d923a42cc54c31411110b22b2dc855af7
Haohan Liu <haohan@outlook.com.au>
2020-10-04 13:01:55 +1100

    Change document title to 'Epic Portfolio'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,13 @@ import UserProfile from './containers/UserProfile'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import RegisterPage from './containers/RegisterPage'
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
+    useEffect(() => {
+        document.title = "Epic Portfolio"
+    }, []);
+
     return (
         <Router>
             <Switch>
