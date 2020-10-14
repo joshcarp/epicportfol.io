2b864e1af437823421c6584a657c4147f23b3c1a
Angus Farquhar <mr.angus.farquhar@gmail.com>
2020-10-14 21:35:57 +1100

    add nav to user profile page

--- frontend/src/containers/UserProfile.js
+++ frontend/src/containers/UserProfile.js
@@ -1,67 +1,71 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom'
 import Timeline from '../components/Timeline'
 
-import {
-    makeStyles,
-    Grid,
-    Paper,
-} from '@material-ui/core'
+import { makeStyles, Grid, Paper } from '@material-ui/core'
 import UserInfoCard from '../components/UserInfoCard'
 import ImageBox from '../components/ImageBox'
 import EditModal from '../components/EditModal'
 import PopModal from '../components/PopModal'
+import Nav from '../containers/Nav'
 
-const { profilesClient } = require('./../proto/api_grpc_web_pb.js');
-const profiles = new profilesClient('https://profiles.epicportfol.io');
-const { getuserRequest, profile } = require('./../proto/api_pb.js');
-const yaml = require('js-yaml');
+const { profilesClient } = require('./../proto/api_grpc_web_pb.js')
+const profiles = new profilesClient('https://profiles.epicportfol.io')
+const { getuserRequest, profile } = require('./../proto/api_pb.js')
+const yaml = require('js-yaml')
 
 export default function UserProfile(props) {
-    const classes = useStyles();
-    let { username } = useParams();
-    const [prof, setProfile] = useState(null);
-    console.log(username);
+    const classes = useStyles()
+    let { username } = useParams()
+    const [prof, setProfile] = useState(null)
+    console.log(username)
     useEffect(() => {
-        var req = new getuserRequest();
-        req.setUserid(username);
+        var req = new getuserRequest()
+        req.setUserid(username)
         profiles.getuser(req, {}, function (err, response) {
             console.log(err)
             console.log(response.toObject())
-            if (err == null){
+            if (err == null) {
                 setProfile(response.toObject())
             }
         })
-
     }, [])
 
     if (prof == null) {
-        return <div>Loading...</div>;
+        return <div>Loading...</div>
     }
     return (
-        <Grid container className={classes.root}>
-            <Grid container component={Paper} className={classes.profile} elevation={4}>
-            <button onClick={() => PopModal()}> test</button>
-                <Grid item className={classes.card}>
-                    <UserInfoCard profile={prof} />
+        <>
+            <Nav />
+            <Grid container className={classes.root}>
+                <Grid
+                    container
+                    component={Paper}
+                    className={classes.profile}
+                    elevation={4}
+                >
+                    <button onClick={() => PopModal()}> test</button>
+                    <Grid item className={classes.card}>
+                        <UserInfoCard profile={prof} />
+                    </Grid>
+                    <Grid item className={classes.card}>
+                        <Timeline profile={prof} />
+                    </Grid>
+                    <Grid item className={classes.card}>
+                        <ImageBox profile={prof} />
+                    </Grid>
+                    <EditModal profile={prof} setProfile={setProfile} />
                 </Grid>
-                <Grid item className={classes.card}>
-                    <Timeline profile={prof} />
-                </Grid>
-                <Grid item className={classes.card}>
-                    <ImageBox profile={prof} />
-                </Grid>
-                <EditModal profile={prof} setProfile={setProfile}/>
+                <Grid className={classes.footer} />
             </Grid>
-            <Grid className={classes.footer} />
-        </Grid>
+        </>
     )
 }
 
 // CSS Theming
 const useStyles = makeStyles((theme) => ({
     root: {
-        minHeight: "100%",
+        minHeight: '100%',
         justifyContent: 'center',
         backgroundColor: 'rgb(50, 50, 50)',
         backgroundImage: 'url("/home-bg.jpg")',
@@ -75,8 +79,8 @@ const useStyles = makeStyles((theme) => ({
         flexWrap: 'wrap',
         alignItems: 'center',
         justifyContent: 'center',
-        maxWidth: "80vmin",
-        minWidth: "60vmin",
+        maxWidth: '80vmin',
+        minWidth: '60vmin',
         marginTop: 40,
         marginBottom: 60,
     },
@@ -92,5 +96,4 @@ const useStyles = makeStyles((theme) => ({
     footer: {
         height: 20,
     },
-}));
-
+}))
