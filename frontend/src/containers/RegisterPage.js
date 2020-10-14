55c17794c68f221bd3583a820e1191449079cc50
Angus Farquhar <mr.angus.farquhar@gmail.com>
2020-10-14 21:25:13 +1100

    add nav to register page

--- frontend/src/containers/RegisterPage.js
+++ frontend/src/containers/RegisterPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { Paper } from "@material-ui/core";
+import { Paper } from '@material-ui/core'
 
 import '../App.css'
 import Logo from '../assets/logo.svg'
 
 import UserRegisterForm from '../components/UserRegisterForm'
+import Nav from './Nav.js'
 
 class RegisterPage extends Component {
     state = {
@@ -12,14 +13,17 @@ class RegisterPage extends Component {
     }
     render() {
         return (
-            <div className="Homepage">
-                <img src={Logo} className="Homepage-logo" alt="logo" />
-                <Paper elevation={3} >
-                    <h4>Register your Epic Portfolio</h4>
-                    <UserRegisterForm />
-                    <br />
-                </Paper>
-            </div>
+            <>
+                <Nav />
+                <div className="Homepage">
+                    <img src={Logo} className="Homepage-logo" alt="logo" />
+                    <Paper elevation={3}>
+                        <h4>Register your Epic Portfolio</h4>
+                        <UserRegisterForm />
+                        <br />
+                    </Paper>
+                </div>
+            </>
         )
     }
 }
