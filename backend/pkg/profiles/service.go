08358a3825a250549c93e5cca8e41227dfe5d279
joshcarp <joshuacarpeggiani@gmail.com>
2020-10-16 22:55:24 +1100

    Add feature flag for profile auth

--- backend/pkg/profiles/service.go
+++ backend/pkg/profiles/service.go
@@ -1,6 +1,8 @@
 package profiles
 
 import (
+	"os"
+
 	"github.com/joshcarp/it-project/backend/internal/config"
 	"github.com/joshcarp/it-project/backend/internal/database"
 	"github.com/joshcarp/it-project/backend/internal/proto/itproject"
@@ -12,6 +14,7 @@ type Server struct {
 	config config.Config
 	db     database.Server
 	log    *logrus.Logger
+	auth   bool
 	itproject.UnimplementedProfilesServer
 }
 
@@ -20,7 +23,7 @@ func NewServer(config config.Config, log *logrus.Logger) (*Server, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Server{config: config, db: db, log: log}, nil
+	return &Server{config: config, db: db, log: log, auth: os.Getenv("AUTHENTICATE") != "FALSE"}, nil
 }
 
 func RegisterService(conf config.Config, log *logrus.Logger, s *grpc.Server) error {
