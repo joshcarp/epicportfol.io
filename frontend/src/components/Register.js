d2af845586d76bb10de9a3fe2e782845219480ef
Joshua Carpeggiani <32605850+joshcarp@users.noreply.github.com>
2020-10-16 21:30:30 +1100

    Fix problems with frontend and firestore (#109)

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,7 +4,7 @@ import '../App.css';
 import { useState } from 'react'
 const { User } = require('../proto/api_pb.js');
 const { authenticateClient } = require('../proto/api_grpc_web_pb.js');
-const auth = new authenticateClient('http://localhost:8081');
+const auth = new authenticateClient('https://authenticate.epicportfol.io');
 
 
 
