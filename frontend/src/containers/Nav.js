2c66023e292d5d6a09f2dcfff8664f3d734ed7b6
Haohan Liu <haohan@outlook.com.au>
2020-11-19 15:44:09 +1100

    Fix some widths and nav styling

--- frontend/src/containers/Nav.js
+++ frontend/src/containers/Nav.js
@@ -3,13 +3,7 @@ import { fade, makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
-import Badge from '@material-ui/core/Badge'
-import MenuItem from '@material-ui/core/MenuItem'
-import Menu from '@material-ui/core/Menu'
 import AccountCircle from '@material-ui/icons/AccountCircle'
-import MailIcon from '@material-ui/icons/Mail'
-import NotificationsIcon from '@material-ui/icons/Notifications'
-import MoreIcon from '@material-ui/icons/MoreVert'
 import { Home } from '@material-ui/icons'
 import NavSearchBox from '../components/NavSearchBox'
 import LogoutButton from '../components/LogoutButton'
@@ -65,23 +59,12 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'flex',
-        },
-    },
-    sectionMobile: {
         display: 'flex',
-        [theme.breakpoints.up('md')]: {
-            display: 'none',
-        },
     },
 }))
 
 export default function PrimarySearchAppBar() {
     const classes = useStyles()
-    const [anchorEl, setAnchorEl] = React.useState(null)
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null)
 
     // Check authentication
     const [isLoggedIn, setIsLoggedIn] = React.useState(false)
@@ -93,86 +76,6 @@ export default function PrimarySearchAppBar() {
         console.log("TOKEN: %o", localStorage.getItem('token'))
         console.log("LOGIN STATE: %o", isLoggedIn)
     }, [isLoggedIn])
-
-    const isMenuOpen = Boolean(anchorEl)
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
-
-    const handleProfileMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget)
-    }
-
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null)
-    }
-
-    const handleMenuClose = () => {
-        setAnchorEl(null)
-        handleMobileMenuClose()
-    }
-
-    // const handleMobileMenuOpen = (event) => {
-    //     setMobileMoreAnchorEl(event.currentTarget)
-    // }
-
-    const menuId = 'primary-search-account-menu'
-    const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-        </Menu>
-    )
-
-    const mobileMenuId = 'primary-search-account-menu-mobile'
-    const renderMobileMenu = (
-        <Menu
-            anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={mobileMenuId}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMobileMenuOpen}
-            onClose={handleMobileMenuClose}
-        >
-            <MenuItem>
-                <IconButton aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={4} color="secondary">
-                        <MailIcon />
-                    </Badge>
-                </IconButton>
-                <p>Messages</p>
-            </MenuItem>
-            <MenuItem>
-                <IconButton
-                    aria-label="show 11 new notifications"
-                    color="inherit"
-                >
-                    <Badge badgeContent={11} color="secondary">
-                        <NotificationsIcon />
-                    </Badge>
-                </IconButton>
-                <p>Notifications</p>
-            </MenuItem>
-            <MenuItem onClick={handleProfileMenuOpen}>
-                <IconButton
-                    aria-label="account of current user"
-                    aria-controls="primary-search-account-menu"
-                    aria-haspopup="true"
-                    color="inherit"
-                >
-                    <AccountCircle />
-                </IconButton>
-                <p>Profile</p>
-            </MenuItem>
-        </Menu>
-    )
 
     return (
         <div className={classes.grow}>
@@ -199,7 +102,6 @@ export default function PrimarySearchAppBar() {
                                         <IconButton
                                             edge="end"
                                             aria-label="account of current user"
-                                            aria-controls={menuId}
                                             aria-haspopup="true"
                                             href={"/u/" + localStorage.getItem("currentUser")}
                                             color="inherit"
@@ -212,21 +114,8 @@ export default function PrimarySearchAppBar() {
                         }
 
                     </div>
-                    <div className={classes.sectionMobile}>
-                        <IconButton
-                            aria-label="show more"
-                            aria-controls={mobileMenuId}
-                            aria-haspopup="true"
-                            href="/"
-                            color="inherit"
-                        >
-                            <MoreIcon />
-                        </IconButton>
-                    </div>
                 </Toolbar>
             </AppBar>
-            {renderMobileMenu}
-            {renderMenu}
         </div>
     )
 }
